81b3f41de12762d279f788f8969a330dfff62f54
pala <pala@tmapy.cz>
2022-10-12 05:39:08 +0200

    feat: LocaleSwitcher

---
+++ src/lib/locale-switcher/LocaleSwitcher.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { _, locale, locales } from 'svelte-i18n';
+
+  const getLanguageName = (lang: string) => {
+    return new Intl.DisplayNames([lang], { type: 'language' }).of(lang);
+  };
+</script>
+
+<div class="locale-selector">
+  <select bind:value={$locale}>
+    {#each $locales as locale}
+      <option value={locale}>{getLanguageName(locale)}</option>
+    {/each}
+  </select>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,28 +1,26 @@
 <script lang="ts" context="module">
-  import { waitLocale } from 'svelte-i18n';
-
   import '../i18n.js';
-
-  export async function preload() {
-    // awaits for the loading of the dictionaries
-    return waitLocale();
-  }
+  import '../app.css';
 </script>
 
 <script lang="ts">
   import { _, isLoading } from 'svelte-i18n';
 
-  import '../app.css';
+  import LocaleSwitcher from '$lib/locale-switcher/LocaleSwitcher.svelte';
 </script>
 
-<!-- {#if !$isLoading} -->
-<nav>
-  <a href="/">{$_('nav.home')}</a>
-  <a href="/about">{$_('nav.about')}</a>
-  <a href="/settings">{$_('nav.settings')}</a>
-</nav>
+{#if $isLoading}
+  ⌛
+{:else}
+  <LocaleSwitcher />
 
-<main>
-  <slot />
-</main>
-<!-- {/if} -->
+  <nav>
+    <a href="/">{$_('nav.home')}</a>
+    <a href="/about">{$_('nav.about')}</a>
+    <a href="/settings">{$_('nav.settings')}</a>
+  </nav>
+
+  <main>
+    <slot />
+  </main>
+{/if}
